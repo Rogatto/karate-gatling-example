8fbc76d2aa5d0f7923c2a388d2503ae1d876aea8
Guilherme-Sensedia <guilherme.rogatto@sensedia.com>
2019-04-10 22:05:54 -0300

    testes paralelos com karate

---
+++ src/test/java/runner/ParallelRunner.java
@@ -0,0 +1,19 @@
+package runner;
+
+import com.intuit.karate.KarateOptions;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+@KarateOptions(tags = {"~@ignore"},features = {
+        "src/test/java/movie/"})
+public class ParallelRunner {
+
+    @Test
+    public void testParallel() {
+        Results results = Runner.parallel(getClass(), 5, "target/surefire-reports");
+        assertTrue(results.getErrorMessages(), results.getFailCount() == 0);
+    }
+
+}
