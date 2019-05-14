b1b11753dc2c99fdc48e5a342488adb9e1e8c5c6
Guilherme-Sensedia <guilherme.rogatto@sensedia.com>
2019-05-14 18:20:22 -0300

    teste

--- src/test/java/movie/MovieSimulation.scala
+++ src/test/java/movie/MovieSimulation.scala
@@ -14,8 +14,9 @@ class MovieSimulation extends Simulation {
   /*protocol.nameResolver = (req, ctx) => req.getHeader("karate-name")*/
 
   val create = scenario("Teste para criar novo filme").exec(karateFeature("classpath:movie/create-movie.feature"))
-
+  val update = scenario("Teste para alterar filme").exec(karateFeature("classpath:movie/update-movie.feature"))
   setUp(
-    create.inject(rampUsers(4) over (5 seconds)).protocols(protocol)
+    create.inject(rampUsers(4) over (5 seconds)).protocols(protocol),
+    update.inject(rampUsers(4) over (5 seconds)).protocols(protocol)
   )
 }
